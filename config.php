64fe757d23cb2e034f9b15b7805d9ff39e1daf1a
Anselmo Scalise <hyristo@MacBook-Pro-di-Anselmo-Scalise.local>
2012-09-22 12:31:14 +0200

    primo commit

---
+++ config.php
@@ -0,0 +1,26 @@
+ <?php /*
+		// Author:- Harminder Virk
+		// Version :- 1.0 
+		// Project Name :- Quiz Engine script under GPL license 
+		// You have the right to modify,use and re-distribute this script
+		// without informing anyone.
+		// As a curtusey you can keep my name there as it is.
+		*/
+		/* Name of the database */
+		define('DB_NAME', 'QUIZ_APOSTOLICO');
+		/* Name of the database user */
+		define('DB_USER', 'root');
+		/* Password for the database */
+		define('DB_PASSWORD', '');
+		/* Host name for the database */
+		define('DB_HOST', 'localhost');
+		/* Default Charset */
+		define('DB_CHARSET', 'utf8');
+		/* Default Collate */
+		define('DB_COLLATE', ''); 
+		/* Facebook Configration */
+		// --- Facebook Share Link --- //
+		define('FACEBOOK_LINK','http://www.thetutlage.com');
+		// --- Facebook Share Query --- //
+		define('FACEBOOK_QUERY','Jquery Quiz Engine Results (www.thetutlage.com)');
+		?>
